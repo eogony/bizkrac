e4d8f61cc42ca592895c66b1b82f3fc8229d4228
eogony <eogony@gmail.com>
2019-08-02 19:17:12 +0300

    created interface to hold data types of User class

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -10,40 +10,49 @@ import { User } from 'firebase';
 })
 export class AuthService {
     /*add variable to store logged in user data*/
-    user: User;
+    userData: User;
 
     /*inject the Firebase authentication service and the router via the service's constructor:*/
 
- constructor(public afAuth: AngularFireAuth, public router: Router) {
+ constructor(
+     public afAuth: AngularFireAuth,
+     public router: Router,
+     public ngZone: NgZone  // NgZone service to remove outside scope warning
+     ) {
      /* in the constructor, we subscribe to the authentication state; if the user is logged in
-        we add the user's data to the browser's local storage; otherwise we store a null user*/
+        we save user data to the browser's local storage; otherwise we store a null user when logged out*/
      this.afAuth.authState.subscribe(user => {
          if (user) {
-             this.user = user;
-             localStorage.setItem('user', JSON.stringify(this.user));
+             this.userData = user;
+             localStorage.setItem('user', JSON.stringify(this.userData));
+             JSON.parse(localStorage.getItem('user'));
          } else {
              localStorage.setItem('user', null);
+             JSON.parse(localStorage.getItem('user'));
          }
      });
  }
 /*add the login() method that will be used to login users with email and password:*/
- /*async login(email: string, password: string) {
-    try {
-        await this.afAuth.auth.signInWithEmailAndPassword(email, password);
-        this.router.navigate(['/']);
-    } catch (e) {
-        window.alert('Error!' + e.message);
-    }
- }*/
+ async login(email: string, password: string) {
+    await this.afAuth.auth.signInWithEmailAndPassword(email, password)
+        .then((result) => {
+            this.ngZone.run(() => {
+                this.router.navigate(['/']);
+            });
+            this.SetUserData(result.user);
+        }).catch((e) => {
+            window.alert('Error!' + e.message);
+        });
+ }
 
-  async login(credential) {
+  /*async login(credential) {
     try {
         await this.afAuth.auth.signInWithCredential(credential);
         this.router.navigate(['/']);
     } catch (e) {
         window.alert('Error!' + e.message);
     }
-  }
+  }*/
 
 
   /*add logout method
