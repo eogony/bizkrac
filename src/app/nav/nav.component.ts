d09fd02ec061c00c480178a2bbe9d49c51a9f375
eogony <eogony@gmail.com>
2019-08-21 21:59:11 +0300

    improved login dropdown

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { User } from 'firebase';
 import { AuthService } from '../services/auth.service';
-import { Observable } from 'rxjs';
 
 @Component({
   // tslint:disable-next-line:component-selector
